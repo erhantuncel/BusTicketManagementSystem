1d186eac6f371f38c4e773a52b678dfb00ef11f5
Erhan TUNCEL <erhan.glstrme@gmail.com>
2019-06-17 01:08:49 +0300

    Fixed: After admin delete ticket income price for voyage does not update.

--- src/main/java/com/erhan/onlinebilet/controller/admin/AdminTicketController.java
+++ src/main/java/com/erhan/onlinebilet/controller/admin/AdminTicketController.java
@@ -1,5 +1,7 @@
 package com.erhan.onlinebilet.controller.admin;
 
+import java.math.BigDecimal;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.erhan.onlinebilet.model.Income;
+import com.erhan.onlinebilet.model.Ticket;
+import com.erhan.onlinebilet.service.IncomeService;
 import com.erhan.onlinebilet.service.TicketService;
 
 @Controller
@@ -17,14 +22,22 @@ public class AdminTicketController {
 	
 	@Autowired
 	TicketService ticketService;
+	
+	@Autowired
+	IncomeService incomeService;
 
 	@RequestMapping(value="/admin/bilet/{id}/sil", method = RequestMethod.GET)
 	public ModelAndView deleteTicket(@PathVariable(value = "id") String id, HttpServletRequest request, RedirectAttributes redir) {
+		Ticket ticket = ticketService.findById(new Long(id));
 		int result = ticketService.delete(new Long(id));
 		String referer = request.getHeader("Referer");
 		ModelAndView model = new ModelAndView("redirect:" + referer); 
 		String resultMessage = null;
 		if(result > 0) {			
+			Income voyageIncome = incomeService.findByVoyage(ticket.getVoyage());
+			BigDecimal newVoyageIncomePrice = voyageIncome.getPrice().subtract(ticket.getPrice());
+			voyageIncome.setPrice(newVoyageIncomePrice);
+			incomeService.update(voyageIncome);
 			resultMessage = "" + id + " numaralı bilet iptal edildi.";
 			redir.addFlashAttribute("warningType", "info");
 		} else {
